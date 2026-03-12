17264769bbe8f83d0e474e5b8bc75b810136a7d0
Kauan Silva <kauanmacedo338@gmail.com>
2026-03-12 16:13:08 -0300

    atividade 3

---
+++ Atividade 3/Atividade3/Program.cs
@@ -0,0 +1,38 @@
+﻿while (true)
+{
+    Console.Clear();
+
+    Console.WriteLine("========================================");
+    Console.WriteLine("Calculadora De Consumo De Combustivel");
+    Console.WriteLine("========================================");
+
+    Console.ReadLine();
+
+    Console.Write("Quilometragem Inicial: ");
+    decimal kmInicial = Convert.ToDecimal(Console.ReadLine());
+
+    Console.Write("Quilometragem Final: ");
+    decimal kmfinal = Convert.ToDecimal(Console.ReadLine());
+
+    Console.Write("quantidade de combustível consumida durante a viagem (em litros): ");
+    decimal ltViagem = Convert.ToDecimal(Console.ReadLine());
+
+    decimal distancia = kmInicial - kmfinal;
+
+    decimal percurso = distancia / ltViagem;
+
+    Console.ReadLine();
+
+    Console.WriteLine($"A distancia percorrida e: {distancia} km");
+
+    Console.WriteLine($"o consumo e: {percurso:F2} km/L");
+
+    Console.ReadLine();
+
+    Console.Write("DESEJA CONTINUAR? s/N ");
+    string? desejaContinuar = Console.ReadLine();
+
+    if (desejaContinuar?.ToUpper() != "S")
+        break;
+
+}
