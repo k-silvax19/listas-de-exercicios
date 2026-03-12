ab0fe3ff27ac47b5174eb1726fe0a450e0946c23
Kauan Silva <kauanmacedo338@gmail.com>
2026-03-12 16:34:02 -0300

    Atividade4

---
+++ atividade4/Program.cs
@@ -0,0 +1,27 @@
+﻿while (true)
+{
+    Console.Clear();
+
+    Console.WriteLine("==============================");
+    Console.WriteLine("Converção de Celsius para Fahrenheint");
+    Console.WriteLine("==============================");
+
+    Console.ReadLine();
+
+    Console.Write("Digite os Celsius: ");
+    decimal C = Convert.ToDecimal(Console.ReadLine());
+
+    Console.ReadKey();
+
+    decimal Fahrenheint = (9m / 5m) * C + 32;
+
+    Console.WriteLine($"O resultado da conversão: {Fahrenheint}");
+
+    Console.ReadLine();
+
+    Console.Write("DESEJA CONTINUAR? s/N ");
+    string? desejaContinuar = Console.ReadLine();
+
+    if (desejaContinuar?.ToUpper() != "S")
+        break;
+}
