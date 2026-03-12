dee8e04f8b75f827f0d0285a5633a920843ce76d
Kauan Silva <kauanmacedo338@gmail.com>
2026-03-12 14:22:54 -0300

    first commit

---
+++ Atividade1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Digite o comprimento da caixa: ");
+decimal comprimento = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Digite a largura da caixa: ");
+decimal largura = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Digite a altura da caixa: ");
+decimal altura = Convert.ToDecimal(Console.ReadLine());
+
+decimal resultado = comprimento * largura * altura;
+
+Console.WriteLine(" o resultado e: " + resultado);
