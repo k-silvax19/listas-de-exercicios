c8510e79a660b4aabdb2ec633b799661da268978
Kauan Silva <kauanmacedo338@gmail.com>
2026-03-12 17:03:07 -0300

    atividade5

---
+++ atividade5/Program.cs
@@ -0,0 +1,34 @@
+﻿while (true)
+{
+    Console.Clear();
+
+    Console.WriteLine("========================================");
+    Console.WriteLine("Calculadora de Salario");
+    Console.WriteLine("========================================");
+
+    Console.ReadLine();
+
+    Console.Write("Salario Base: ");
+    decimal Salario = Convert.ToDecimal(Console.ReadLine());
+
+    Console.Write("Total De Vendas: ");
+    decimal Vendas = Convert.ToDecimal(Console.ReadLine());
+
+    Console.Write("Digite o valor da comissão (porcentagem): ");
+    decimal comissao = Convert.ToDecimal(Console.ReadLine());
+
+    decimal salariototal = Salario + (Vendas * (comissao / 100));
+
+    Console.ReadLine();
+
+    Console.WriteLine($"o salario total e:  {salariototal}");
+
+    Console.ReadLine();
+
+    Console.Write("DESEJA CONTINUAR? s/N ");
+    string? desejaContinuar = Console.ReadLine();
+
+    if (desejaContinuar?.ToUpper() != "S")
+        break;
+
+}
