54f4cb8fb3f0b441dd613bf2aa016d24900b0a0e
Kauan Silva <kauanmacedo338@gmail.com>
2026-03-12 15:43:05 -0300

    Calculadora de Volume de Cilindro, com while e melhoria de interacao

---
+++ Atividade 2/Atividade 2/Program.cs
@@ -0,0 +1,31 @@
+﻿while (true)
+{
+    Console.Clear();
+
+    Console.WriteLine("----------------------");
+    Console.WriteLine("Calculadora de Volume Do Cilindro");
+    Console.WriteLine("----------------------");
+
+    Console.ReadLine();
+
+    Console.Write("Digite o Raio: ");
+    decimal Raio = Convert.ToDecimal(Console.ReadLine());
+
+    Console.Write("Digite a Altura: ");
+    decimal Altura = Convert.ToDecimal(Console.ReadLine());
+
+    decimal Pi = (decimal)Math.PI;
+
+    decimal Base = Pi * (decimal)Math.Pow((double)Raio, 2);
+
+    decimal resultado = Base * Altura;
+
+    Console.WriteLine($"O resultado do volume e: {resultado:F2}");
+
+    Console.Write("DESEJA CONTINUAR? s/N ");
+    string? desejaContinuar = Console.ReadLine();
+
+    if (desejaContinuar?.ToUpper() != "S")
+        break;
+
+}
