a0343984ded678706ac35c6c93d70a8211a8ae43
Kauan Silva <kauanmacedo338@gmail.com>
2026-03-12 20:53:20 -0300

    atividade9

---
+++ Atividade9/Program.cs
@@ -0,0 +1,30 @@
+﻿while (true)
+{
+    Console.Clear();
+
+    Console.WriteLine("========================================");
+    Console.WriteLine("Algoritimo para ler dimensoes retangulares! ");
+    Console.WriteLine("========================================");
+
+    Console.ReadLine();
+
+    Console.Write("Comprimento Do Terreno: ");
+    decimal C = Convert.ToDecimal(Console.ReadLine());
+
+    Console.Write("Largura Do Terreno: ");
+    decimal L = Convert.ToDecimal(Console.ReadLine());
+
+    decimal area = C * L;
+
+    Console.ReadLine();
+
+    Console.WriteLine($"A area do terreno e: {area} m²");
+
+    Console.ReadLine();
+
+    Console.Write("DESEJA CONTINUAR? s/N ");
+    string? desejaContinuar = Console.ReadLine();
+
+    if (desejaContinuar?.ToUpper() != "S")
+        break;
+}
