132f8d49150343109e5ace6bf382da504be2d3ab
Kauan Silva <kauanmacedo338@gmail.com>
2026-03-12 15:47:27 -0300

    adicao de while e melhoria na interface

--- Atividade1/Program.cs
+++ Atividade1/Program.cs
@@ -1,12 +1,29 @@
-﻿Console.Write("Digite o comprimento da caixa: ");
-decimal comprimento = Convert.ToDecimal(Console.ReadLine());
+﻿while (true)
+{
+    Console.Clear();
 
-Console.Write("Digite a largura da caixa: ");
-decimal largura = Convert.ToDecimal(Console.ReadLine());
+    Console.WriteLine("==============================");
+    Console.WriteLine("Calculadora de Volume de Caixa Retangular");
+    Console.WriteLine("==============================");
 
-Console.Write("Digite a altura da caixa: ");
-decimal altura = Convert.ToDecimal(Console.ReadLine());
+    Console.ReadLine();
 
-decimal resultado = comprimento * largura * altura;
+    Console.Write("Digite o comprimento da caixa: ");
+    decimal comprimento = Convert.ToDecimal(Console.ReadLine());
 
-Console.WriteLine($" o resultado e: {resultado}");
+    Console.Write("Digite a largura da caixa: ");
+    decimal largura = Convert.ToDecimal(Console.ReadLine());
+
+    Console.Write("Digite a altura da caixa: ");
+    decimal altura = Convert.ToDecimal(Console.ReadLine());
+
+    decimal resultado = comprimento * largura * altura;
+
+    Console.WriteLine($" o resultado e: {resultado}");
+
+    Console.Write("DESEJA CONTINUAR? s/N ");
+    string? desejaContinuar = Console.ReadLine();
+
+    if (desejaContinuar?.ToUpper() != "S")
+        break;
+}
