5b382881a3226de8ec0267047d3e3c9800de9cab
Kauan Silva <kauanmacedo338@gmail.com>
2026-03-13 15:42:47 -0300

    Atividade19

---
+++ Atividade19/Program.cs
@@ -0,0 +1,29 @@
+﻿while (true)
+{
+    Console.Clear();
+
+    Console.WriteLine("===============================");
+    Console.WriteLine("Soma dos Numeros Impares");
+    Console.WriteLine("===============================");
+
+    Console.ReadLine();
+
+    int soma = 0;
+
+    for (int contador = 1; contador <= 500; contador++)
+    {
+        if (contador % 2 != 0 && contador % 3 == 0)
+        {
+            soma += contador;
+            Console.WriteLine(contador);
+        }
+    }
+
+    Console.ReadLine();
+
+    Console.Write("DESEJA CONTINUAR? s/N ");
+    string? desejaContinuar = Console.ReadLine();
+
+    if (desejaContinuar?.ToUpper() != "S")
+        break;
+}
