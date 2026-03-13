308d3bbb7a40010f617b2607c568dba635df0d25
Kauan Silva <kauanmacedo338@gmail.com>
2026-03-12 21:31:56 -0300

    avitivade11

---
+++ atividade11/Program.cs
@@ -0,0 +1,25 @@
+﻿while (true)
+{
+    Console.Clear();
+
+    Console.WriteLine("==============================");
+    Console.WriteLine("Calculadora de Volume de Anos de Vida");
+    Console.WriteLine("==============================");
+
+    Console.ReadLine();
+
+    Console.Write("Digite o seus anos de vida: ");
+    decimal anosVida = Convert.ToDecimal(Console.ReadLine());
+
+    decimal resultado = anosVida * 365;
+
+    Console.WriteLine($"o resultado dos anos de vida é: {resultado}");
+
+    Console.ReadLine();
+
+    Console.Write("DESEJA CONTINUAR? s/N ");
+    string? desejaContinuar = Console.ReadLine();
+
+    if (desejaContinuar?.ToUpper() != "S")
+        break;
+}
