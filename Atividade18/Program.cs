05aba7c3ec17b4e0546b01d53e626cdb7e3e2c48
Kauan Silva <kauanmacedo338@gmail.com>
2026-03-13 15:33:15 -0300

    Atividade18

---
+++ Atividade18/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+while (true)
+{
+    Console.Clear();
+
+    Console.WriteLine("===============================");
+    Console.WriteLine("Mostrar os números ímpares entre 100 e 200");
+    Console.WriteLine("===============================");
+
+    Console.ReadLine();
+
+    for (int contador = 100; contador <= 200; contador++)
+    {
+        if (contador % 2 != 0)
+        {
+            Console.WriteLine(contador);
+        }
+    }
+
+    Console.ReadLine();
+
+    Console.Write("DESEJA CONTINUAR? s/N ");
+    string? desejaContinuar = Console.ReadLine();
+
+    if (desejaContinuar?.ToUpper() != "S")
+        break;
+}
