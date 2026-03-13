7ab36204ef60842110cc5eeb47c5187acd38bf36
Kauan Silva <kauanmacedo338@gmail.com>
2026-03-13 15:02:52 -0300

    Atividade16

---
+++ Atividade16/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Xml.Serialization;
+
+while (true)
+{
+    Console.Clear();
+
+    Console.WriteLine("==============================");
+    Console.WriteLine("Impar Ou Par");
+    Console.WriteLine("==============================");
+
+    Console.Write("Digite o seu numero: ");
+    int numero = Convert.ToInt32(Console.ReadLine());
+
+    Console.ReadLine();
+
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine("O numero e par");
+    }
+    else
+    {
+        Console.WriteLine("O numero e impar");
+    }
+
+    Console.ReadLine();
+
+
+    Console.Write("DESEJA CONTINUAR? s/N ");
+    string? desejaContinuar = Console.ReadLine();
+
+    if (desejaContinuar?.ToUpper() != "S")
+        break;
+
+}
