6a5ce789d05c80dc33cb9007fe2edf548455f4e3
Kauan Silva <kauanmacedo338@gmail.com>
2026-03-13 16:54:24 -0300

    Atividade23

---
+++ Atividade23/Program.cs
@@ -0,0 +1,39 @@
+﻿while (true)
+{
+    Console.Clear();
+
+    Console.WriteLine("===============================");
+    Console.WriteLine("FizzBuzz");
+    Console.WriteLine("===============================");
+
+    Console.Write("Digite o seu número: ");
+    int numero = Convert.ToInt32(Console.ReadLine());
+
+    for (int contador = 1; contador <= numero; contador++)
+    {
+        if (contador % 3 == 0 && contador % 5 == 0)
+        {
+            Console.WriteLine("FizzBuzz");
+        }
+        else if (contador % 3 == 0)
+        {
+            Console.WriteLine("Fizz");
+        }
+        else if (contador % 5 == 0)
+        {
+            Console.WriteLine("Buzz");
+        }
+        else
+        {
+            Console.WriteLine(contador);
+        }
+    }
+
+    Console.WriteLine();
+
+    Console.Write("DESEJA CONTINUAR? s/N ");
+    string? desejaContinuar = Console.ReadLine();
+
+    if (desejaContinuar?.ToUpper() != "S")
+        break;
+}
