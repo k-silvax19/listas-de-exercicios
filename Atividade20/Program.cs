08884c60745f7d455dd22555a3c8cdf6eb457bb3
Kauan Silva <kauanmacedo338@gmail.com>
2026-03-13 15:56:01 -0300

    Atividade20

---
+++ Atividade20/Program.cs
@@ -0,0 +1,31 @@
+﻿while (true)
+{
+    Console.Clear();
+
+    Console.WriteLine("===============================");
+    Console.WriteLine("Tabuada");
+    Console.WriteLine("===============================");
+
+    Console.ReadLine();
+
+    Console.Write("Digite o numero da tabuada: ");
+    int numeroTabuada = Convert.ToInt32(Console.ReadLine());
+
+    for (int contador = 0; contador <= 10; contador++)
+    {
+        int resultadoTabuada = numeroTabuada * contador;
+
+        string linhaTabuada = contador + " x " + numeroTabuada + " = " + resultadoTabuada;
+
+        Console.WriteLine(linhaTabuada);
+
+    }
+
+    Console.ReadLine();
+    
+    Console.Write("DESEJA CONTINUAR? s/N ");
+    string? desejaContinuar = Console.ReadLine();
+     if (desejaContinuar?.ToUpper() != "S")
+        break;
+
+}
